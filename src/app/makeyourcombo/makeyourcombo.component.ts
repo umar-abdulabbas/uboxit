d771478502436190fda5bf5c0ef454d3842a407b
umar-addulabbas <text.umarabbas@gmail.com>
2017-10-20 17:37:42 +0200

    Anchor check

--- src/app/makeyourcombo/makeyourcombo.component.ts
+++ src/app/makeyourcombo/makeyourcombo.component.ts
@@ -29,11 +29,43 @@ export class MakeyourcomboComponent implements OnInit, OnDestroy{
     let width = window.getComputedStyle(this.MenuContainerWidth, null).getPropertyValue('width'); 
     console.log(width);
     
-   let ItemLengtth = document.getElementsByClassName('item').length; 
+    let padLeft = document.getElementById('LeftPad');
+    let widthpadLeft = window.getComputedStyle(padLeft, null).getPropertyValue('width'); 
+    console.log(widthpadLeft);
+
+    let rightPad = document.getElementById('rightPad');
+    let widthrightPad = window.getComputedStyle(padLeft, null).getPropertyValue('width'); 
+    console.log(widthrightPad);
+
+
+    let ItemLengtth = document.getElementsByClassName('item').length; 
     console.log(ItemLengtth);   
    
-    let itemSize = document.getElementsByClassName('item')[0].clientWidth;
-     console.log(itemSize);   
+    let itemSize = document.getElementsByClassName('item')[0];
+    let widthElem = window.getComputedStyle(itemSize, null).getPropertyValue('width'); 
+    console.log(widthElem);   
+
+    function findMenuItemLength(classNames){
+        let ItemLengtth = document.getElementsByClassName(classNames).length; 
+        //return console.log(ItemLengtth);
+        var storeValue = [];
+        function getSum(total, num) {
+            return total + num;
+        }
+        for(let i=0; i < ItemLengtth; i++){
+          let count:any = 0;
+          let totalWidth =  document.getElementsByClassName('item')[i];
+          let widthElem = window.getComputedStyle(itemSize, null).getPropertyValue('width');
+          var reg = widthElem.replace('px','');    
+          storeValue.push(reg);
+        }
+        var b = storeValue.map(Number);  
+       // console.log(b);
+        return b.reduce(getSum);
+    }
+     var z =  findMenuItemLength('item'); 
+    console.log(z);
+
   }
   ngOnDestroy(){
     this.headerColor.classList.remove("headerFixedShoppingCard");

55b2012bc898ca93a8bb52e240f31723b1f3d1ac
Umar Abbas <umar432@gmail.com>
2018-02-19 18:32:30 +0100

    bar-menu.component has been updated for dynamic menu

--- src/app/components/shared/bar-menu/bar-menu.component.ts
+++ src/app/components/shared/bar-menu/bar-menu.component.ts
@@ -1,6 +1,22 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { OfferService } from '../../offers/services/offer.service';
+import { Observable } from 'rxjs/Observable';
+/* Menu Start */
+export class menuList{
+  id:string;
+  name:string;
+  link:string;
+  icon:string;
+  filter:boolean;
+  menuActive:boolean;
+}
 
+const MENULIST:menuList[]=[
+    { id:"001", name:"Combo", link:'/home', icon:"restaurant_menu", filter:true, menuActive:true},
+    { id:"002", name:"Make Combo",link:'/home', icon:"room_service", filter:false, menuActive:false},
+    { id:"003", name:"Items", link:'/home', icon:"restaurant", filter:true, menuActive:false}
+]
+/* Menu End */
 @Component({
   selector: 'app-bar-menu',
   templateUrl: './bar-menu.component.html',
@@ -12,7 +28,10 @@ export class BarMenuComponent implements OnInit {
   public isBtnActive = true;
   public pathFinder: string;
   public isActiveDropDown = false;
-
+  public showTwoCol = false;
+  public showThreeCol = false;
+  menuList = MENULIST;
+  totalCount: Observable<any>;
   @Input() availableTypes: string[] = [];
   @Output() filterType = new EventEmitter<string>();
 
@@ -25,12 +44,21 @@ export class BarMenuComponent implements OnInit {
       this.isActive = true;
       this.isBtnActive = false;
     }
+    this.showHideColClass();
   }
-
+  showHideColClass() {
+    if(menuList.length === 2){
+     return this.showTwoCol = true;
+      
+    }
+    if(menuList.length === 3){
+      return this.showThreeCol = true;
+    }
+  }
   selectType(type: string): void {
     console.log(type);
     this.filterType.emit(type);
-    
+    this.isActiveDropDown = false;
   }
   showDropDown(): void {
     this.isActiveDropDown = !this.isActiveDropDown;

b04cb470f961626c5c77e377818cab66640ef1b5
Malaiappan <malaiappan.kannan@gmail.com>
2018-02-20 19:45:19 +0100

    before redirect to payment clear cart

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { PaymentService } from './services/payment-service';
 import { Observable } from 'rxjs/Observable';
 import { Cart } from '../../core/domain/cart';
+import { StorageService } from '../../shared/services/storage-service';
 
 @Component({
   selector: 'app-payment',
@@ -16,7 +17,8 @@ export class PaymentComponent implements OnInit {
     context : 'test' // change it to `live` when going live.
   };
 
-  constructor(private paymentService: PaymentService) { }
+  constructor(private paymentService: PaymentService,
+              private storageService: StorageService) { }
 
   ngOnInit() {
 
@@ -29,10 +31,10 @@ export class PaymentComponent implements OnInit {
           adyenSdk['checkout'](res, '#checkout-div', this.sdkConfigObj);
         });
 
-      adyenSdk['hooks']['beforeComplete'] = (node, paymentData) => {
-        console.log(node);
-        console.log(paymentData);
-        return false;
+      adyenSdk['hooks']['beforeRedirect'] = () => {
+        console.log('before redirect');
+        this.storageService.clearCart();
+        return true;
       };
     });
   }

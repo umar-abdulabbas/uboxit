aac5c5e16485124a16b1b996b9e021f27ccbafd8
Malaiappan <malaiappan.kannan@gmail.com>
2018-03-10 22:33:21 +0100

    config service - idea - yet to be filled

---
+++ src/app/core/config/config.service.ts
@@ -0,0 +1,7 @@
+// this file holds all the current config of uboxit
+// consider moving it outside of this application
+// e.g CMS, admin-tool, apis
+
+export class ConfigService {
+  // all the configs needs to be moved here (eg pickup at store addres, category names translation)
+}

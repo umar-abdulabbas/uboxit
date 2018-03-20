f19edbb04e3e3ce97405260e07018dd04c812af4
Malaiappan <malaiappan.kannan@gmail.com>
2018-03-20 13:39:06 +0530

    PUT handling for make your combo

--- src/app/components/shoppingcart/ordered-items/ordered-items.component.ts
+++ src/app/components/shoppingcart/ordered-items/ordered-items.component.ts
@@ -22,17 +22,25 @@ export class OrderedItemsComponent implements OnInit {
   }
 
   updateOrder(productId: string, count: number) {
-    this.cartService.addComboToCart(productId, count); // for cart @ header top right
-    this.offerService.updateCountForCombo(productId, count); // just for UI - will not be used for processing cart - for couter with value
-    const request: Cart = { combos: [ { id: productId, count: count } ] };
-    this.cartService.updateCart(this.cart.id, request).subscribe((updatedCart) => {
-      this.cart = updatedCart;
-      const totalCount = this.cart.combos ? this.cart.combos.map(c => c.count).reduce((prev, cur) => prev + cur, 0) : 0;
-      if (totalCount === 0) {
-        console.log('All your orders are cancelled, you can start over again!!');
-        this.router.navigate(['/home']);
-      }
-    });
+    // another hack
+    if (!productId.toLowerCase().startsWith('c')) {
+      this.cartService.addComboToCart(productId, count); // for cart @ header top right
+      this.offerService.updateCountForCombo(productId, count); // just for UI - will not be used for processing cart - for couter with value
+      const request: Cart = { combos: [ { id: productId, count: count } ] };
+      this.cartService.updateCart(this.cart.id, request).subscribe((updatedCart) => {
+        this.cart = updatedCart;
+        const totalCount = this.cart.combos ? this.cart.combos.map(c => c.count).reduce((prev, cur) => prev + cur, 0) : 0;
+        if (totalCount === 0) {
+          console.log('All your orders are cancelled, you can start over again!!');
+          this.router.navigate(['/home']);
+        }
+      });
+    } else {
+      const combo = this.cart.combos.find(c => c.id === productId);
+      const itemIds = combo.items.map(i => i.id);
+      this.cartService.prepareCustomisedCombo(itemIds, count);
+      this.cartService.updateCartWithCustomisedCombo();
+    }
   }
 
   removeOrder(productId: string) {

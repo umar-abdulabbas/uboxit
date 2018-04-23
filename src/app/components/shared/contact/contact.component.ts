29c8ff5a394e4cbce9a6d85fb4c2b896040a44a6
Umar <umar432@gmail.com>
2018-04-23 19:25:19 +0200

    UI fixes -> Images are now in cloud

--- src/app/components/shared/contact/contact.component.ts
+++ src/app/components/shared/contact/contact.component.ts
@@ -8,13 +8,22 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  fburl = "https://www.facebook.com/uboxit.nl";
+  instagram = "https://www.instagram.com/uboxit.nl";
+  twitter = "https://twitter.com/uboxit_nl";
+  target = "_blank";
   constructor(private uistyleservice: UserExpStyleService, private titleService: Title) {
   }
-
+  
   ngOnInit() {
     this.titleService.setTitle('Event and Party Order, Contact us to get a better deal for events and party orders');
     this.uistyleservice.scrollToTop();
+    if(this.uistyleservice.getDeviceInformation()) {
+      this.fburl = "";
+      this.instagram = "";
+      this.twitter ="";
+      this.target = "";
+    }
   }
 
 }
